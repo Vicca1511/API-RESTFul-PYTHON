acf95dfc5fd3d425ee0be444cf681038f25d88a1
Lucas Viccari <97442300+Vicca1511@users.noreply.github.com>
2025-09-28 21:35:42 -0300

    fix: fixing Enum and orthography

--- app/main.py
+++ app/main.py
@@ -1,15 +1,47 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException 
+from app.database import livros_db, get_proximo_id, get_livros_count, get_livros_disponiveis
+from typing import List, Dict, Any
 
-app= FastAPI(
-    title="Sistema Biblioteca API",
-    description="API para gerenciamento de uma biblioteca ",
-    version="1.0.0"
+
+app = FastAPI(
+title="Sistema Biblioteca API",
+description = "Api de gerenciamento de biblioteca",
+version="1.0.0"
 )
 
-@app.get("/home")
+@app.get("/")
 async def root():
-    return {"message":"Bem-vindo a API da Biblioteca"}
+    return {"message":"Bem-vindo ao Sistema Bibliote API"}
 
 @app.get("/health")
 async def health_check():
-    return{"status":"API rodando normalmente!"}
+    return {"status":"Api rodando normalmente!"}
+
+@app.get("/info")
+async def info():
+    """informações sobre estado da biblioteca"""
+    return  {
+        "total_livros":get_livros_count(),
+        "livros_disponiveis": len(get_livros_disponiveis()),
+        "status":"operacional"
+                 
+    }                    
+            
+@app.get("/livros", response_model =List[Dict[str, Any]])
+async def listar_livros():
+    """Lista de Livros cadastrados"""
+    return list(livros_db.values())
+
+@app.get("/livros/{livros_id}")
+async def buscar_livro(livro_id:int):
+    """Busca livro especifico pelo ID"""
+    livro = livros_db.get(livro_id)
+    if not livro:
+        raise HTTPException(status_code=404, detail = "Livro não encontrado")    
+    
+    return livro 
+
+@app.get("/livros/disponiveis")
+async def listar_livros_disponiveis():
+    """Retorna Livros que tem disponibilidade"""
+    return get_livros_disponiveis()
--- app/schemas.py
+++ app/schemas.py
@@ -3,11 +3,11 @@ from typing import Optional
 from enum import Enum
 
 class CategoriaLivro(str, Enum):
-    FICCAO:"ficcao"
-    TECNICO:"tecnico"
-    CIENTIFICO:"cientifico"
-    BIOGRAFIA:"biografia"
-    HISTORIA:"historia"
+    FICCAO = "ficcao"
+    TECNICO = "tecnico"
+    CIENTIFICO = "cientifico"
+    BIOGRAFIA = "biografia"
+    HISTORIA = "historia"
 
 class LivroBase(BaseModel):
     titulo:str
@@ -15,18 +15,18 @@ class LivroBase(BaseModel):
     categoria:CategoriaLivro
     ano_publicacao: int
     isbn:Optional[str] = None
-    diaponivel: bool = True
+    disponivel: bool = True
 
 class LivroCreate(LivroBase):
     pass
 
-class Livroupdate(BaseModel):
+class LivroUpdate(BaseModel):
     titulo:str = None
     autor:str = None
     categoria:CategoriaLivro = None
     ano_publicacao: int = None
     isbn:Optional[str] = None
-    diaponivel: bool = None
+    disponivel: bool = None
 
 class Livro(LivroBase):
     id:int
