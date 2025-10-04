19843167507d7240245bd99f67654c5e7c37f362
Lucas Viccari <97442300+Vicca1511@users.noreply.github.com>
2025-10-04 19:56:31 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,47 +1,52 @@
-from fastapi import FastAPI, HTTPException 
-from app.database import livros_db, get_proximo_id, get_livros_count, get_livros_disponiveis
-from typing import List, Dict, Any
-
+from fastapi import FastAPI , HTTPException , status
+from app.schemas import Livro , LivroCreate , LivroUpdate
+from app.crud import crud_livros
+from typing import List
 
 app = FastAPI(
-title="Sistema Biblioteca API",
-description = "Api de gerenciamento de biblioteca",
-version="1.0.0"
+    title= "Sistea Biclioteca API",
+    description= " API para gerenciamento de biblioteca",
+    version="1.0.0"
 )
 
 @app.get("/")
 async def root():
-    return {"message":"Bem-vindo ao Sistema Bibliote API"}
+    return {"message":"Bem Vindo a API da Biblioteca!"}
 
 @app.get("/health")
 async def health_check():
-    return {"status":"Api rodando normalmente!"}
+    return {"message":"API rodando normalmente."}
 
 @app.get("/info")
 async def info():
-    """informações sobre estado da biblioteca"""
-    return  {
-        "total_livros":get_livros_count(),
-        "livros_disponiveis": len(get_livros_disponiveis()),
-        "status":"operacional"
-                 
-    }                    
-            
-@app.get("/livros", response_model =List[Dict[str, Any]])
+        """Informações sobre o estado da biblioteca"""
+        livros = crud_livros.listar_livros()
+        disponiveis = [livro for livro in livros if livro.disponivel]
+        return {
+        "total_livros": len(livros),
+        "livros_disponiveis": len(disponiveis),
+        "status": "operacional"
+    }
+
+
+# --- ENDPOINTS CRUD PARA LIVROS ---
+
+@app.get("/livros", response_model= List[Livro])
 async def listar_livros():
-    """Lista de Livros cadastrados"""
-    return list(livros_db.values())
+     """Lista todos os livros cadastrados"""
+     return crud_livros.listar_livros()
 
-@app.get("/livros/{livros_id}")
+@app.get("/livros/{livro_id}", response_model= Livro)
 async def buscar_livro(livro_id:int):
-    """Busca livro especifico pelo ID"""
-    livro = livros_db.get(livro_id)
-    if not livro:
-        raise HTTPException(status_code=404, detail = "Livro não encontrado")    
-    
-    return livro 
+     """Busca um livro específico pelo ID"""
+     livro = crud_livros.buscar_livro(livro_id)
+     if not livro:
+          raise HTTPException(
+               status_code= status.HTTP_404_NOT_FOUND,
+               detail= "Livro não encontrado"
+          )
+     return livro
 
-@app.get("/livros/disponiveis")
-async def listar_livros_disponiveis():
-    """Retorna Livros que tem disponibilidade"""
-    return get_livros_disponiveis()
+
+     
+    
